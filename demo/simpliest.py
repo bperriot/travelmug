bfddbf6a58e77f6d430b02a6563370e36c2dea42
Bruno Perriot <bperriot@gmail.com>
2017-10-08 18:16:15 -0700

    initial commit

---
+++ demo/simpliest.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import travelmug
+
+mug = travelmug.TravelMug()
+
+
+@mug.add
+def addition(number_1, number_2):
+    """Add 2 intergers"""
+    return int(number_1) + int(number_2)
+
+
+@mug.add
+def sub(number_1, number_2):
+    """Return number_1 - number_2"""
+    return int(number_1) - int(number_2)
+
+
+mug.run(debug=True)
---
+++ setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+
+setup(
+    name='Travelmug',
+    version='0.0.1',
+    description='Python web app on the go',
+    url='https://github.com/bperriot/travelmug',
+    author='Bruno Perriot',
+    author_email='bperriot@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: User Interfaces',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.3',
+        # 'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 3.5',
+    ],
+    keywords='web flask ui',
+    packages=['travelmug', ],
+    install_requires=['Flask', 'Flask-Bootstrap'],
+    package_data={
+        'travelmug': [
+            'index.html',
+            'webfunc.html',
+        ],
+    },
+    long_description=open('README.md').read(),
+)
